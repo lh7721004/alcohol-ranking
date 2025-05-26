21de81516c75b4d3bc849586612e12614902665e
lh7721004 <lh7721004@gmail.com>
2025-05-26 21:25:49 +0900

    update: 메인 페이지 퍼블리싱 수정 및 API 연결 준비

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,19 +1,19 @@
+import { useNavigate } from "react-router-dom"
+import axios from "axios";
 export default function Main(){
+    const navigate = useNavigate();
+    const logout = ()=>{
+        console.log("Logout Logic 연결");
+        navigate('/login');
+    }
     return (
-    <div className="flex flex-row h-full justify-center">
-        <div className="flex flex-col gap-1">
-            <div className={"text-4xl font-bold"}>관리자 페이지</div>
-            <div className="relative">
-                <input placeholder="학번" className="pr-14"/>
-                <img className="absolute right-1 top-1/2 -translate-y-1/2" src="/src/assets/account.png"/>
+        <div className="h-[100vh] w-full">
+            <img src="/src/assets/signout.png" className="absolute right-0 top-0" onClick={logout}/>
+            <div className="flex flex-col h-full w-full justify-center px-3 gap-3">
+                <div className="px-8 py-8 bg-[#F7CAA0] rounded-xl text-center hover:bg-[#FFC300]" onClick={()=>{navigate('/bottleinput')}}>입력하기</div>
+                <div className="px-8 py-8 bg-[#F7CAA0] rounded-xl text-center hover:bg-[#FFC300]" onClick={()=>{navigate('/qr')}}>최근 QR 표시</div>
+                <div className="px-8 py-8 bg-[#F7CAA0] rounded-xl text-center hover:bg-[#FFC300]" onClick={()=>{navigate('/')}}>이전 내역 조회</div>
             </div>
-            <input placeholder="비밀번호"/>
-            <div>
-                <input type={"checkbox"} id={"checkbox"}/>
-                <label htmlFor="checkbox" className="cursor-pointer">내 정보 기억하기</label>
-            </div>
-            <button className="bg-[#FFA940]">LOGIN</button>
         </div>
-    </div>
     )
 }
