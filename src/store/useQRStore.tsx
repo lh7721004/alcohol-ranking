d968b7e92964de6c5504fa9bb790b1706d9ed30d
lh7721004 <lh7721004@gmail.com>
2025-05-28 03:33:41 +0900

    update: QRCode 전역 상태관리 시간 추가

--- src/store/useQRStore.tsx
+++ src/store/useQRStore.tsx
@@ -1,9 +1,13 @@
 import { create } from 'zustand';
 type QRStore = {
     QRcode: string;
-    setQRcode: (count: string) => void;
+    QRTime: Date;
+    setQRcode: (QRcode: string) => void;
+    setQRTime: (QRTime: Date) => void;
 };
 export const useQRStore = create<QRStore>((set) => ({
     QRcode: "",
+    QRTime: undefined,
     setQRcode: (QRcode) => set({ QRcode }),
+    setQRTime: (QRTime) => set({ QRTime })
 }));
